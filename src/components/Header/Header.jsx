b03e1628b9c1964fe2bd186e68442b5edd4cbf45
eabelloc <eabelloc.dev@gmail.com>
2022-11-22 05:20:20 +0100

    minor updates

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,31 +1,68 @@
-import React, { useContext } from 'react'
-import { RickAndMortyContext } from '../../context/context'
-import './Header.css'
+import React, { useContext } from "react";
+import { RickAndMortyContext } from "../../context/context";
+import "./Header.css";
 
-const Header = ({setShowCharacters, setShowLocations, setShowEpisodes}) => {
-  const { login, logout, user } = useContext(RickAndMortyContext)
+const Header = ({ setShowCharacters, setShowLocations, setShowEpisodes }) => {
+  const { login, logout, user } = useContext(RickAndMortyContext);
 
   return (
     <header>
-        <nav>
-              <ul>
-                <li><a href="https://rickandmortyapi.com/">Rick-and-Morty-API</a></li>
-                <li><button onClick={() => setShowCharacters(true) & setShowLocations(false) & setShowEpisodes(false)}>Characters</button></li>
-                <li><button onClick={() => setShowLocations(true) & setShowCharacters(false) & setShowEpisodes(false)}>Locations</button></li>
-                <li><button onClick={() => setShowEpisodes(true) & setShowCharacters(false) & setShowLocations (false)}>Episodes</button></li>
-                {user != undefined ? (
-                <>
-                <li><button onClick={() => logout()}>Logout</button></li>
-                <h3>Welcome {user}!🥳</h3>
-                </>
-                ) : (
-                <li><button onClick={() => login()}>Login</button></li>
-                )}
-
-              </ul>
-        </nav>
+      <nav>
+        <ul>
+          <li>
+            <a href="https://rickandmortyapi.com/" target="_blank">
+              Rick-and-Morty-API
+            </a>
+          </li>
+          <li>
+            <button
+              onClick={() =>
+                setShowCharacters(true) &
+                setShowLocations(false) &
+                setShowEpisodes(false)
+              }
+            >
+              Characters
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={() =>
+                setShowLocations(true) &
+                setShowCharacters(false) &
+                setShowEpisodes(false)
+              }
+            >
+              Locations
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={() =>
+                setShowEpisodes(true) &
+                setShowCharacters(false) &
+                setShowLocations(false)
+              }
+            >
+              Episodes
+            </button>
+          </li>
+          {user != undefined ? (
+            <>
+              <li>
+                <button onClick={() => logout()}>Logout</button>
+              </li>
+              <h3>Welcome {user}!🥳</h3>
+            </>
+          ) : (
+            <li>
+              <button onClick={() => login()}>Login</button>
+            </li>
+          )}
+        </ul>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
